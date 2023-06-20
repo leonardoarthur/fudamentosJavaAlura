b6aa97e007b43c53b790258e279873651a739cc8
Leonardo Arthur <lraguiar.la@gmail.com>
2023-06-19 21:43:58 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+
+        System.out.println("O ano de lançamento do filme é: " + ano);
+
+        boolean incluidoNoPlano = true;
+
+        double notaDoFilme = 8.1;
+    }
+}
