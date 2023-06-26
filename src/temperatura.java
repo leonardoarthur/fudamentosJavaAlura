eb053a4151f1f96c0b1f7bd1855351b217d69d9b
Leonardo Arthur <lraguiar.la@gmail.com>
2023-06-25 22:43:35 -0300

    desafio  temperatura

---
+++ src/temperatura.java
@@ -0,0 +1,19 @@
+public class temperatura {
+    public static void main(String[] args) {
+
+        //Conversor de temperatura C° para Fahrenheit.
+
+        double temp;
+        double celsius = 10;
+
+
+        temp = (celsius * 1.8) + 32;
+
+        System.out.println("O valor em Celsius é "+ celsius + "F°, em Fahrenheit fica " + temp + "C°");
+
+
+    }
+
+
+
+}
